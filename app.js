d1c90c20ff8dde632c978a53fe1f48c93889bb9f
ziphoncayiyana <ziphoncayiyana@gmail.com>
2023-06-26 14:46:12 +0200

    update

---
+++ app.js
@@ -0,0 +1,65 @@
+const container = document.querySelector('.container')
+const btnBlack = document.createElement('button')
+const btnRgb = document.createElement('button')
+const btnGray = document.createElement('button')
+const btnSize = document.createElement('button')
+const section = document.querySelector('.section');
+const buttonsContainer = document.querySelector('.buttons');
+
+
+function creatDivs(col , rows) {
+    for(let i = 0;i < (col * rows); i++) {
+        const div = document.createElement('div')
+        div.style.border = '1px solid red'; 
+        container.style.gridTemplateColumns = `repeat(${col}, 1fr)`;
+        container.style.gridTemplateRows = `repeat(${rows}, 1fr)`;
+        container.appendChild(div).classList.add('box')
+    }
+}
+creatDivs(16,16)
+
+
+function grayColor() { 
+    
+    const boxs = container.querySelectorAll('.box')
+    btnGray.textContent = "GRAY"
+    btnGray.addEventListener('click', () => {
+        boxs.forEach(box => box.addEventListener('mouseover', ()=> {
+            let RNum = Math.floor(Math.random() * 256);
+            let GrayScale = `rgb(${RNum},${RNum},${RNum})`
+            box.style.background = GrayScale;
+        }))
+    })
+    buttonsContainer.appendChild(btnGray).classList.add('btn')
+}
+grayColor()
+
+function blackColor() { 
+    
+    const boxs = container.querySelectorAll('.box')
+    btnBlack.textContent = "BLACK"
+    btnBlack.addEventListener('click', () => {
+        boxs.forEach(box => box.addEventListener('mouseover', ()=> {
+            box.style.background='black';
+        }));
+    });
+    buttonsContainer.appendChild(btnBlack).classList.add('btn');
+}
+blackColor()
+
+function rgbColors() {
+    
+    const boxs = container.querySelectorAll('.box')
+    btnRgb.textContent = "RGB"
+    btnRgb.addEventListener('click' ,() => {
+        boxs.forEach(box => box.addEventListener('mouseover', () => {
+            let R = Math.floor(Math.random() * 256);
+            let G = Math.floor(Math.random() * 256);
+            let B = Math.floor(Math.random() * 256);
+            const RGB = `rgb(${R},${G},${B})`;
+            box.style.background = RGB;
+        }));
+    });
+    buttonsContainer.appendChild(btnRgb).classList.add('btn')
+}
+rgbColors()
